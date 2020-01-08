960750a216b882437a4485cccf8bbcb33f540fd9
Ajal <ajapoos1999@gmail.com>
2020-01-08 15:47:50 +0530

    Updated

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const UserDetails = require('./models/userDetails.model');
+const userdetails = require('./models/userDetails.model');
+
+const data_routes = require("./routes/data.route");
 
 // Establish a database connection
-mongoose.connect('mongodb://localhost/ca');
+mongoose.connect('mongodb://localhost/ca', {
+    useMongoClient : true,
+});
 
 //Create server
-let server = express();
+const server = express();
 
 //Use assets
 server.use("/assets/css", express.static(__dirname + "/assets/css"));
+server.use("/view",data_routes);
 
 //Setting view engine
 server.set("view engine","ejs");
@@ -19,20 +23,6 @@ server.set("view engine","ejs");
 //Show static page
 server.get('/',(req,res) => {
         res.render('index');
-});
-
-server.get('/view',(req,res) => {  
-    //Get the database
-    let db = mongoose.connection;   
-    db.on('error',console.error.bind(console,"connection error"));
-    db.once('open', () => {
-        console.log("Connection Successful");
-        UserDetails.find((err,docs) => {
-            if(err) return console.error(err);
-            console.log(docs);
-        });
-    });
-
 });
 
 server.listen(3000);
